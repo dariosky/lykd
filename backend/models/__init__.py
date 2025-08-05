400c79c63ba3d293e6e042589d5604e7498b963c
Dario Varotto <dario.varotto@gsr.io>
2025-08-05 23:35:04 +0200

    favicon and some model syntax fix

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,4 +1,19 @@
 """Models package for LYKD backend"""
 
-from .auth import *  # noqa
-from .music import *  # noqa
+from .common import get_session, CamelModel
+from .auth import User
+from .music import Artist, Track, TrackArtist, AlbumArtist, Liked, Play, Album
+
+__all__ = [
+    "User",
+    "Artist",
+    "Track",
+    "TrackArtist",
+    "Album",
+    "Play",
+    "Liked",
+    "AlbumArtist",
+    "AlbumArtist",
+    "get_session",
+    "CamelModel",
+]
